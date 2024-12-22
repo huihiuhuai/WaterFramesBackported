99bdca9331e3dc5b6542cc9c2c253e6b28138b24
Toshayo <abc126cba.abc126cba@yandex.ru>
2024-12-22 20:07:03 +0100

    . Added fatal logging downgrade and moved log4j downgrade helper functions to separate file for readability

---
+++ src/main/java/net/toshayo/waterframes/transformers/Log4JDowngradeHelper.java
@@ -0,0 +1,409 @@
+package net.toshayo.waterframes.transformers;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+
+public class Log4JDowngradeHelper {
+
+    public static void info(Logger logger, String message, Object p0) {
+        logger.info(message, p0);
+    }
+
+    public static void info(Logger logger, String message, Object p0, Object p1) {
+        logger.info(message, p0, p1);
+    }
+
+    public static void info(Logger logger, String message, Object p0, Object p1, Object p2) {
+        logger.info(message, p0, p1, p2);
+    }
+
+    public static void info(Logger logger, String message, Object p0, Object p1, Object p2, Object p3) {
+        logger.info(message, p0, p1, p2, p3);
+    }
+
+    public static void info(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4) {
+        logger.info(message, p0, p1, p2, p3, p4);
+    }
+
+    public static void info(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5) {
+        logger.info(message, p0, p1, p2, p3, p4, p5);
+    }
+
+    public static void info(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6) {
+        logger.info(message, p0, p1, p2, p3, p4, p5, p6);
+    }
+
+    public static void info(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7) {
+        logger.info(message, p0, p1, p2, p3, p4, p5, p6, p7);
+    }
+
+    public static void info(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8) {
+        logger.info(message, p0, p1, p2, p3, p4, p5, p6, p7, p8);
+    }
+
+    public static void info(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8, Object p9) {
+        logger.info(message, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9);
+    }
+
+    public static void info(Logger logger, Marker marker, String message, Object p0) {
+        logger.info(marker, message, p0);
+    }
+
+    public static void info(Logger logger, Marker marker, String message, Object p0, Object p1) {
+        logger.info(marker, message, p0, p1);
+    }
+
+    public static void info(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2) {
+        logger.info(marker, message, p0, p1, p2);
+    }
+
+    public static void info(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3) {
+        logger.info(marker, message, p0, p1, p2, p3);
+    }
+
+    public static void info(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4) {
+        logger.info(marker, message, p0, p1, p2, p3, p4);
+    }
+
+    public static void info(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5) {
+        logger.info(marker, message, p0, p1, p2, p3, p4, p5);
+    }
+
+    public static void info(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6) {
+        logger.info(marker, message, p0, p1, p2, p3, p4, p5, p6);
+    }
+
+    public static void info(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7) {
+        logger.info(marker, message, p0, p1, p2, p3, p4, p5, p6, p7);
+    }
+
+    public static void info(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8) {
+        logger.info(marker, message, p0, p1, p2, p3, p4, p5, p6, p7, p8);
+    }
+
+    public static void info(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8, Object p9) {
+        logger.info(marker, message, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9);
+    }
+
+    public static void warn(Logger logger, String message, Object p0) {
+        logger.warn(message, p0);
+    }
+
+    public static void warn(Logger logger, String message, Object p0, Object p1) {
+        logger.warn(message, p0, p1);
+    }
+
+    public static void warn(Logger logger, String message, Object p0, Object p1, Object p2) {
+        logger.warn(message, p0, p1, p2);
+    }
+
+    public static void warn(Logger logger, String message, Object p0, Object p1, Object p2, Object p3) {
+        logger.warn(message, p0, p1, p2, p3);
+    }
+
+    public static void warn(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4) {
+        logger.warn(message, p0, p1, p2, p3, p4);
+    }
+
+    public static void warn(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5) {
+        logger.warn(message, p0, p1, p2, p3, p4, p5);
+    }
+
+    public static void warn(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6) {
+        logger.warn(message, p0, p1, p2, p3, p4, p5, p6);
+    }
+
+    public static void warn(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7) {
+        logger.warn(message, p0, p1, p2, p3, p4, p5, p6, p7);
+    }
+
+    public static void warn(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8) {
+        logger.warn(message, p0, p1, p2, p3, p4, p5, p6, p7, p8);
+    }
+
+    public static void warn(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8, Object p9) {
+        logger.warn(message, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9);
+    }
+
+    public static void warn(Logger logger, Marker marker, String message, Object p0) {
+        logger.warn(marker, message, p0);
+    }
+
+    public static void warn(Logger logger, Marker marker, String message, Object p0, Object p1) {
+        logger.warn(marker, message, p0, p1);
+    }
+
+    public static void warn(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2) {
+        logger.warn(marker, message, p0, p1, p2);
+    }
+
+    public static void warn(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3) {
+        logger.warn(marker, message, p0, p1, p2, p3);
+    }
+
+    public static void warn(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4) {
+        logger.warn(marker, message, p0, p1, p2, p3, p4);
+    }
+
+    public static void warn(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5) {
+        logger.warn(marker, message, p0, p1, p2, p3, p4, p5);
+    }
+
+    public static void warn(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6) {
+        logger.warn(marker, message, p0, p1, p2, p3, p4, p5, p6);
+    }
+
+    public static void warn(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7) {
+        logger.warn(marker, message, p0, p1, p2, p3, p4, p5, p6, p7);
+    }
+
+    public static void warn(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8) {
+        logger.warn(marker, message, p0, p1, p2, p3, p4, p5, p6, p7, p8);
+    }
+
+    public static void warn(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8, Object p9) {
+        logger.warn(marker, message, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9);
+    }
+
+    public static void error(Logger logger, String message, Object p0) {
+        logger.error(message, p0);
+    }
+
+    public static void error(Logger logger, String message, Object p0, Object p1) {
+        logger.error(message, p0, p1);
+    }
+
+    public static void error(Logger logger, String message, Object p0, Object p1, Object p2) {
+        logger.error(message, p0, p1, p2);
+    }
+
+    public static void error(Logger logger, String message, Object p0, Object p1, Object p2, Object p3) {
+        logger.error(message, p0, p1, p2, p3);
+    }
+
+    public static void error(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4) {
+        logger.error(message, p0, p1, p2, p3, p4);
+    }
+
+    public static void error(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5) {
+        logger.error(message, p0, p1, p2, p3, p4, p5);
+    }
+
+    public static void error(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6) {
+        logger.error(message, p0, p1, p2, p3, p4, p5, p6);
+    }
+
+    public static void error(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7) {
+        logger.error(message, p0, p1, p2, p3, p4, p5, p6, p7);
+    }
+
+    public static void error(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8) {
+        logger.error(message, p0, p1, p2, p3, p4, p5, p6, p7, p8);
+    }
+
+    public static void error(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8, Object p9) {
+        logger.error(message, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9);
+    }
+
+    public static void error(Logger logger, Marker marker, String message, Object p0) {
+        logger.error(marker, message, p0);
+    }
+
+    public static void error(Logger logger, Marker marker, String message, Object p0, Object p1) {
+        logger.error(marker, message, p0, p1);
+    }
+
+    public static void error(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2) {
+        logger.error(marker, message, p0, p1, p2);
+    }
+
+    public static void error(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3) {
+        logger.error(marker, message, p0, p1, p2, p3);
+    }
+
+    public static void error(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4) {
+        logger.error(marker, message, p0, p1, p2, p3, p4);
+    }
+
+    public static void error(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5) {
+        logger.error(marker, message, p0, p1, p2, p3, p4, p5);
+    }
+
+    public static void error(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6) {
+        logger.error(marker, message, p0, p1, p2, p3, p4, p5, p6);
+    }
+
+    public static void error(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7) {
+        logger.error(marker, message, p0, p1, p2, p3, p4, p5, p6, p7);
+    }
+
+    public static void error(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8) {
+        logger.error(marker, message, p0, p1, p2, p3, p4, p5, p6, p7, p8);
+    }
+
+    public static void error(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8, Object p9) {
+        logger.error(marker, message, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9);
+    }
+
+    public static void debug(Logger logger, String message, Object p0) {
+        logger.debug(message, p0);
+    }
+
+    public static void debug(Logger logger, String message, Object p0, Object p1) {
+        logger.debug(message, p0, p1);
+    }
+
+    public static void debug(Logger logger, String message, Object p0, Object p1, Object p2) {
+        logger.debug(message, p0, p1, p2);
+    }
+
+    public static void debug(Logger logger, String message, Object p0, Object p1, Object p2, Object p3) {
+        logger.debug(message, p0, p1, p2, p3);
+    }
+
+    public static void debug(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4) {
+        logger.debug(message, p0, p1, p2, p3, p4);
+    }
+
+    public static void debug(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5) {
+        logger.debug(message, p0, p1, p2, p3, p4, p5);
+    }
+
+    public static void debug(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6) {
+        logger.debug(message, p0, p1, p2, p3, p4, p5, p6);
+    }
+
+    public static void debug(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7) {
+        logger.debug(message, p0, p1, p2, p3, p4, p5, p6, p7);
+    }
+
+    public static void debug(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8) {
+        logger.debug(message, p0, p1, p2, p3, p4, p5, p6, p7, p8);
+    }
+
+    public static void debug(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8, Object p9) {
+        logger.debug(message, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9);
+    }
+
+    public static void debug(Logger logger, Marker marker, String message, Object p0) {
+        logger.debug(marker, message, p0);
+    }
+
+    public static void debug(Logger logger, Marker marker, String message, Object p0, Object p1) {
+        logger.debug(marker, message, p0, p1);
+    }
+
+    public static void debug(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2) {
+        logger.debug(marker, message, p0, p1, p2);
+    }
+
+    public static void debug(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3) {
+        logger.debug(marker, message, p0, p1, p2, p3);
+    }
+
+    public static void debug(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4) {
+        logger.debug(marker, message, p0, p1, p2, p3, p4);
+    }
+
+    public static void debug(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5) {
+        logger.debug(marker, message, p0, p1, p2, p3, p4, p5);
+    }
+
+    public static void debug(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6) {
+        logger.debug(marker, message, p0, p1, p2, p3, p4, p5, p6);
+    }
+
+    public static void debug(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7) {
+        logger.debug(marker, message, p0, p1, p2, p3, p4, p5, p6, p7);
+    }
+
+    public static void debug(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8) {
+        logger.debug(marker, message, p0, p1, p2, p3, p4, p5, p6, p7, p8);
+    }
+
+    public static void debug(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8, Object p9) {
+        logger.debug(marker, message, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9);
+    }
+
+
+
+    public static void fatal(Logger logger, String message, Object p0) {
+        logger.fatal(message, p0);
+    }
+
+    public static void fatal(Logger logger, String message, Object p0, Object p1) {
+        logger.fatal(message, p0, p1);
+    }
+
+    public static void fatal(Logger logger, String message, Object p0, Object p1, Object p2) {
+        logger.fatal(message, p0, p1, p2);
+    }
+
+    public static void fatal(Logger logger, String message, Object p0, Object p1, Object p2, Object p3) {
+        logger.fatal(message, p0, p1, p2, p3);
+    }
+
+    public static void fatal(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4) {
+        logger.fatal(message, p0, p1, p2, p3, p4);
+    }
+
+    public static void fatal(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5) {
+        logger.fatal(message, p0, p1, p2, p3, p4, p5);
+    }
+
+    public static void fatal(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6) {
+        logger.fatal(message, p0, p1, p2, p3, p4, p5, p6);
+    }
+
+    public static void fatal(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7) {
+        logger.fatal(message, p0, p1, p2, p3, p4, p5, p6, p7);
+    }
+
+    public static void fatal(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8) {
+        logger.fatal(message, p0, p1, p2, p3, p4, p5, p6, p7, p8);
+    }
+
+    public static void fatal(Logger logger, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8, Object p9) {
+        logger.fatal(message, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9);
+    }
+
+    public static void fatal(Logger logger, Marker marker, String message, Object p0) {
+        logger.fatal(marker, message, p0);
+    }
+
+    public static void fatal(Logger logger, Marker marker, String message, Object p0, Object p1) {
+        logger.fatal(marker, message, p0, p1);
+    }
+
+    public static void fatal(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2) {
+        logger.fatal(marker, message, p0, p1, p2);
+    }
+
+    public static void fatal(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3) {
+        logger.fatal(marker, message, p0, p1, p2, p3);
+    }
+
+    public static void fatal(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4) {
+        logger.fatal(marker, message, p0, p1, p2, p3, p4);
+    }
+
+    public static void fatal(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5) {
+        logger.fatal(marker, message, p0, p1, p2, p3, p4, p5);
+    }
+
+    public static void fatal(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6) {
+        logger.fatal(marker, message, p0, p1, p2, p3, p4, p5, p6);
+    }
+
+    public static void fatal(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7) {
+        logger.fatal(marker, message, p0, p1, p2, p3, p4, p5, p6, p7);
+    }
+
+    public static void fatal(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8) {
+        logger.fatal(marker, message, p0, p1, p2, p3, p4, p5, p6, p7, p8);
+    }
+
+    public static void fatal(Logger logger, Marker marker, String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8, Object p9) {
+        logger.debug(marker, message, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9);
+    }
+}
--- src/main/java/net/toshayo/waterframes/transformers/Log4JDowngradingVisitor.java
+++ src/main/java/net/toshayo/waterframes/transformers/Log4JDowngradingVisitor.java
@@ -20,17 +20,17 @@ public class Log4JDowngradingVisitor extends ClassVisitor {
         return new MethodVisitor(Opcodes.ASM5, methodVisitor) {
             @Override
             public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
-                boolean isReplacableMethod = name.equals("info") || name.equals("warn") || name.equals("error") || name.equals("debug");
+                boolean isReplacableMethod = name.equals("info") || name.equals("warn") || name.equals("error") || name.equals("debug") || name.equals("fatal");
                 if (opcode == Opcodes.INVOKEINTERFACE &&
                         owner.equals("org/apache/logging/log4j/Logger") && isReplacableMethod &&
                         desc.startsWith("(Lorg/apache/logging/log4j/Marker;Ljava/lang/String;Ljava/lang/Object;")) {
                     WaterFramesPlugin.LOGGER.info("Patching {}.{} in {}.{}{}", "Logger", name, transformedName, methodName, desc);
-                    mv.visitMethodInsn(Opcodes.INVOKESTATIC, "net/toshayo/waterframes/PluginUtils", name, "(Lorg/apache/logging/log4j/Logger;" + desc.substring(1), false);
+                    mv.visitMethodInsn(Opcodes.INVOKESTATIC, "net/toshayo/waterframes/transformers/Log4JDowngradeHelper", name, "(Lorg/apache/logging/log4j/Logger;" + desc.substring(1), false);
                 } else if (opcode == Opcodes.INVOKEINTERFACE &&
                         owner.equals("org/apache/logging/log4j/Logger") && isReplacableMethod &&
                         desc.startsWith("(Ljava/lang/String;Ljava/lang/Object;")) {
                     WaterFramesPlugin.LOGGER.info("Patching {}.{} in {}.{}{}", "Logger", name, transformedName, methodName, desc);
-                    mv.visitMethodInsn(Opcodes.INVOKESTATIC, "net/toshayo/waterframes/PluginUtils", name, "(Lorg/apache/logging/log4j/Logger;" + desc.substring(1), false);
+                    mv.visitMethodInsn(Opcodes.INVOKESTATIC, "net/toshayo/waterframes/transformers/Log4JDowngradeHelper", name, "(Lorg/apache/logging/log4j/Logger;" + desc.substring(1), false);
                 } else {
                     super.visitMethodInsn(opcode, owner, name, desc, itf);
                 }
